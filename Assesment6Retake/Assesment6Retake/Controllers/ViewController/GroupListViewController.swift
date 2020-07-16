cba8a4ef431a9eff8588290feaf27ed231bdc894
Connor Holland <connorholland@Connors-MacBook-Pro.local>
2020-07-16 10:20:32 -0600

    Created UI and viewcontroller file

---
+++ Assesment6Retake/Assesment6Retake/Controllers/ViewController/GroupListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GroupListViewController.swift
+//  Assesment6Retake
+//
+//  Created by Connor Holland on 7/16/20.
+//  Copyright © 2020 Connor Holland. All rights reserved.
+//
+
+import UIKit
+
+class GroupListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+} 
